27da8bc5b6e6d95b0c82f36882b3172065315c72
nagadurgakiran <nagadurgakiran@gmail.com>
2018-12-17 22:36:26 +0530

    Latest file updating -First checkin

---
+++ GenerateScriptsforNodes/GenerateScriptsforNodes/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace GenerateScriptsforNodes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsers = new System.Windows.Forms.TextBox();
+            this.txtNodeCode = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtScript = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblUserNames = new System.Windows.Forms.Label();
+            this.lblSiteID = new System.Windows.Forms.Label();
+            this.lblAmounts = new System.Windows.Forms.Label();
+            this.txtAmounts = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtUsers
+            // 
+            this.txtUsers.Location = new System.Drawing.Point(74, 43);
+            this.txtUsers.Multiline = true;
+            this.txtUsers.Name = "txtUsers";
+            this.txtUsers.Size = new System.Drawing.Size(466, 109);
+            this.txtUsers.TabIndex = 0;
+            // 
+            // txtNodeCode
+            // 
+            this.txtNodeCode.Location = new System.Drawing.Point(561, 43);
+            this.txtNodeCode.Name = "txtNodeCode";
+            this.txtNodeCode.Size = new System.Drawing.Size(223, 20);
+            this.txtNodeCode.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtScript
+            // 
+            this.txtScript.Location = new System.Drawing.Point(74, 209);
+            this.txtScript.Multiline = true;
+            this.txtScript.Name = "txtScript";
+            this.txtScript.Size = new System.Drawing.Size(1150, 240);
+            this.txtScript.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(192, 171);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblUserNames
+            // 
+            this.lblUserNames.AutoSize = true;
+            this.lblUserNames.Location = new System.Drawing.Point(71, 9);
+            this.lblUserNames.Name = "lblUserNames";
+            this.lblUserNames.Size = new System.Drawing.Size(62, 13);
+            this.lblUserNames.TabIndex = 5;
+            this.lblUserNames.Text = "UserNames";
+            // 
+            // lblSiteID
+            // 
+            this.lblSiteID.AutoSize = true;
+            this.lblSiteID.Location = new System.Drawing.Point(558, 9);
+            this.lblSiteID.Name = "lblSiteID";
+            this.lblSiteID.Size = new System.Drawing.Size(39, 13);
+            this.lblSiteID.TabIndex = 6;
+            this.lblSiteID.Text = "Site ID";
+            // 
+            // lblAmounts
+            // 
+            this.lblAmounts.AutoSize = true;
+            this.lblAmounts.Location = new System.Drawing.Point(805, 9);
+            this.lblAmounts.Name = "lblAmounts";
+            this.lblAmounts.Size = new System.Drawing.Size(48, 13);
+            this.lblAmounts.TabIndex = 7;
+            this.lblAmounts.Text = "Amounts";
+            // 
+            // txtAmounts
+            // 
+            this.txtAmounts.Location = new System.Drawing.Point(808, 43);
+            this.txtAmounts.Multiline = true;
+            this.txtAmounts.Name = "txtAmounts";
+            this.txtAmounts.Size = new System.Drawing.Size(416, 109);
+            this.txtAmounts.TabIndex = 8;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(745, 171);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "User Access";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(628, 171);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "User Group";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(512, 171);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MaskFile";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(394, 171);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "NodeCodeSP";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 487);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtAmounts);
+            this.Controls.Add(this.lblAmounts);
+            this.Controls.Add(this.lblSiteID);
+            this.Controls.Add(this.lblUserNames);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtScript);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNodeCode);
+            this.Controls.Add(this.txtUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsers;
+        private System.Windows.Forms.TextBox txtNodeCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtScript;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblUserNames;
+        private System.Windows.Forms.Label lblSiteID;
+        private System.Windows.Forms.Label lblAmounts;
+        private System.Windows.Forms.TextBox txtAmounts;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ GenerateScriptsforNodes/GenerateScriptsforNodes/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace GenerateScriptsforNodes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ReadExcelData();
+        }
+
+        private void ReadExcelData()
+        {
+            string path = @"E:\testingexcell.xls";
+            IList<NodeDetails> objExcelCon = ReadExcel(path);
+
+            var nodeCodeList = objExcelCon.Select(x => x.Restriction).Distinct().ToList();
+
+            //ExecuteNodeCodes(nodeCodeList);
+
+            var oldNodeCode = "";
+            var newNodeCode = "";
+            var originatorInboxNodeID = 0;
+            var originatorInboxUserNodeID = 0;
+            var nodeCodeApproverNodeID = 0;
+
+            var maskFileScript = "";
+            var userGroupScript = "";
+            var userAccessScript = "";
+
+            for (int i = 0; i < objExcelCon.Count(); i++)
+            {                
+                newNodeCode = objExcelCon[i].Restriction.ToString();
+                if (newNodeCode != "")
+                {
+
+                    if (oldNodeCode == "" || (oldNodeCode != newNodeCode))
+                    {
+                        var NodeCodeDetails = GetNodeCodeDetails(newNodeCode);
+                        originatorInboxNodeID = Convert.ToInt32(NodeCodeDetails.Tables[0].Rows[0][0].ToString());
+                        originatorInboxUserNodeID = Convert.ToInt32(NodeCodeDetails.Tables[0].Rows[1][0].ToString());
+                        nodeCodeApproverNodeID = Convert.ToInt32(NodeCodeDetails.Tables[0].Rows[2][0].ToString());
+
+                        oldNodeCode = newNodeCode;
+
+                        maskFileScript += "--" + newNodeCode + "  -- " + nodeCodeApproverNodeID + " \n";
+                        maskFileScript += "-----------------------------------  \n";
+                        userGroupScript += "--" + newNodeCode + " \n";
+                        userGroupScript += "----------------------------------  \n";
+                        userAccessScript += "--" + newNodeCode + " \n";
+                        userAccessScript += "----------------------------------  \n";
+                    }
+                    if (Convert.ToInt32(objExcelCon[i].AmountLimit) != 0)
+                        maskFileScript += "insert into MaskFile_Hierarchy values('" + objExcelCon[i].UserDetailsID.ToString() + "','" + objExcelCon[i].Restriction.ToString() + "'," + Convert.ToInt32(objExcelCon[i].AmountLimit) + "," + nodeCodeApproverNodeID + ",22); \n";
+
+                    if (Convert.ToInt32(objExcelCon[i].AmountLimit) == 0)
+                        userGroupScript += "insert into workflow_group values(" + originatorInboxUserNodeID + ",'" + objExcelCon[i].UserDetailsID.ToString() + "',0,''); \n";
+                    else
+                        userGroupScript += "insert into workflow_group values(" + nodeCodeApproverNodeID + ",'" + objExcelCon[i].UserDetailsID.ToString() + "',0,''); \n";
+
+                    userAccessScript += "IF NOT EXISTS(SELECT * FROM UserAccess WHERE KEYVALUE = '" + objExcelCon[i].Restriction.ToString() + "' AND USERDETAILSID = '" + objExcelCon[i].UserDetailsID.ToString() + "') \n";
+
+                    userAccessScript += "BEGIN \n";
+
+                    userAccessScript += "Insert Into UserAccess values('" + objExcelCon[i].UserDetailsID.ToString() + "', 231, 38, '" + objExcelCon[i].Restriction.ToString() + "', Convert(varbinary(500), '')) \n";
+
+                    userAccessScript += "END \n";
+                }
+            }
+            maskFileScript += "-----------------------------------  \n";
+            userGroupScript += "----------------------------------  \n";
+            userAccessScript += "----------------------------------  \n";
+
+            //ExcecuteScript(maskFileScript);
+            //ExcecuteScript(userGroupScript);
+            //ExcecuteScript(userAccessScript);
+
+        }
+
+        private DataSet GetNodeCodeDetails(string newNodeCode)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=hydddiser;Initial Catalog=DocAgent_SWIFT;User ID=ddiarchive;Password=CBdcHq2@pm18mvEzr6F5Mmw==?");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "select * from workflow_node where Description like '%" + newNodeCode + "%' order by WorkFlowNodeID ";
+                cmd.Connection = conn;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                //Console.WriteLine("Inserting Data Successfully");
+                conn.Close();
+
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Exception Occre while creating SP:" + e.Message + "\t" + e.GetType());
+            }
+            return ds;
+        }
+
+        private void ExecuteNodeCodes(List<string> nodeCodeList)
+        {
+            if (nodeCodeList.Count() > 0)
+            {
+                var sqlScript = "";
+                for (int i = 0; i < nodeCodeList.Count(); i++)
+                {
+                    sqlScript += "exec [InsertNodeCode_In_Transition_Conditions_new] '" + nodeCodeList[i] + "' \n";
+                    sqlScript += "GO \n";
+                }
+
+                ExcecuteScript(sqlScript);
+
+            }
+        }
+
+        private void ExcecuteScript(string sqlScript)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data source=USER-PC; Database=Emp123;User Id=sa;Password=sa123");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = sqlScript;
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Inserting Data Successfully");
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Occre while creating SP:" + e.Message + "\t" + e.GetType());
+            }
+            Console.ReadKey();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var userList = txtUsers.Text.Trim().Split('\n');
+            var amountList = txtAmounts.Text.Trim().Split('\n');
+            var queryScript = "";
+
+            if (userList.Count() > 0)
+            {
+                queryScript += "\n";
+                queryScript += "-- " + txtNodeCode.Text + " \n";
+                queryScript += "----------------------------------------------- \n";
+
+
+                foreach (var item in userList)
+                {
+                    queryScript += "IF NOT EXISTS(SELECT * FROM UserAccess WHERE KEYVALUE = '" + txtNodeCode.Text + "' AND USERDETAILSID = '" + item + "') \n";
+
+                    queryScript += "BEGIN \n";
+
+                    queryScript += "Insert Into UserAccess values('" + item + "', 231, 38, '" + txtNodeCode.Text + "', Convert(varbinary(500), '')) \n";
+
+                    queryScript += "END \n";
+                }
+            }
+
+            queryScript += "----------------------------------------------- \n";
+            queryScript += "\n";
+
+            txtScript.Text = queryScript;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNodeCode.Text = "";
+            txtScript.Text = "";
+            txtUsers.Text = "";
+        }
+
+        private OleDbConnection OpenConnection(string path)
+        {
+            OleDbConnection oledbConn = null;
+            try
+            {
+                if (Path.GetExtension(path) == ".xls")
+                    oledbConn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + path +
+
+"; Extended Properties= \"Excel 8.0;HDR=Yes;IMEX=2\"");
+                else if (Path.GetExtension(path) == ".xlsx")
+                    oledbConn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" +
+
+path + "; Extended Properties='Excel 12.0;HDR=YES;IMEX=1;';");
+
+                oledbConn.Open();
+            }
+            catch (Exception ex)
+            {
+                //Error
+            }
+            return oledbConn;
+        }
+
+        private IList<NodeDetails> ExtractNodeDetailsExcel(OleDbConnection oledbConn)
+        {
+            OleDbCommand cmd = new OleDbCommand(); ;
+            OleDbDataAdapter oleda = new OleDbDataAdapter();
+            DataSet dsNodeDetailsInfo = new DataSet();
+
+            cmd.Connection = oledbConn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [Node WorkFlow$]"; //Excel Sheet Name ( Employee )
+            oleda = new OleDbDataAdapter(cmd);
+            oleda.Fill(dsNodeDetailsInfo, "NodeDetails");
+
+            var dsEmployeeInfoList = dsNodeDetailsInfo.Tables[0].AsEnumerable().Select(s => new NodeDetails
+            {
+                UserDetailsID = Convert.ToString(s["UserDetailsID"] != DBNull.Value ? s["UserDetailsID"] : ""),
+                FirstName = Convert.ToString(s["FirstName"] != DBNull.Value ? s["FirstName"] : ""),
+                LastName = Convert.ToString(s["LastName"] != DBNull.Value ? s["LastName"] : ""),
+                EmailID = Convert.ToString(s["EmailID"] != DBNull.Value ? s["EmailID"] : ""),
+                Restriction = Convert.ToString(s["Restriction"] != DBNull.Value ? s["Restriction"] : ""),
+                AmountLimit = Convert.ToString(s["AmountLimit"] != DBNull.Value ? s["AmountLimit"] : ""),
+                Role = Convert.ToString(s["Role"] != DBNull.Value ? s["Role"] : ""),
+                Notes = Convert.ToString(s["Notes"] != DBNull.Value ? s["Notes"] : "")
+            }).ToList();
+
+            return dsEmployeeInfoList;
+        }
+
+        public IList<NodeDetails> ReadExcel(string path)
+        {
+            IList<NodeDetails> objNodeDetailsInfo = new List<NodeDetails>();
+            OleDbConnection oledbConn = OpenConnection(path);
+            try
+            {
+
+                if (oledbConn.State == ConnectionState.Open)
+                {
+                    objNodeDetailsInfo = ExtractNodeDetailsExcel(oledbConn);
+                    oledbConn.Close();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // Error
+            }
+
+            return objNodeDetailsInfo;
+        }
+
+    }
+
+    public class NodeDetails
+    {
+        public string UserDetailsID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailID { get; set; }
+        public string Restriction { get; set; }
+        public string AmountLimit { get; set; }
+        public string Role { get; set; }
+        public string Notes { get; set; }
+    }
+}
